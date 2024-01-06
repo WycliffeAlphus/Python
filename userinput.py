22983584813ee8befc256e7e2fa5f633a9af28d2
Wycliffe Alphus <155804225+WycliffeAlphus@users.noreply.github.com>
2024-01-06 14:24:55 +0300

    Add files via upload

---
+++ userinput.py
@@ -0,0 +1,6 @@
+name = input("What is your name?: ")
+age = int(input("How old are you?: "))
+height = float(input("How tall ar you?: "))
+print("Hello "+name)
+print("You are "+ str(age) + " years old")
+print("You are " +str(height)+"cm tall")

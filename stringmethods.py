982fe73f5ac863969e6b62b8e4f7fe0a9897176d
Wycliffe Alphus <155804225+WycliffeAlphus@users.noreply.github.com>
2024-01-06 14:24:14 +0300

    Add files via upload

---
+++ stringmethods.py
@@ -0,0 +1,12 @@
+name = "Wycliffe"
+
+#print(len(name))
+#print(name.find("c"))
+#print(name.capitalize())
+#print(name.upper())
+#print(name.lower())
+#print(name.isdigit())
+#print(name.isalpha())
+#print(name.count("f"))
+#print(name.replace("f", "e"))
+print(name*3)

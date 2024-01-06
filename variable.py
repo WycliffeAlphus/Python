4ca11800f51d9cf3247f51a9e4742e7460849a7a
Wycliffe Alphus <155804225+WycliffeAlphus@users.noreply.github.com>
2024-01-06 14:21:48 +0300

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,19 @@
+# String
+#first_name = "Wyliffe"
+#last_name = "Alphus"
+#full_name = first_name + " " + last_name
+#print(type(first_name))
+#print("Hello " +full_name)
+
+# Integer
+#age = 22
+#age += 1
+#print ("Your age is " + str(age))
+
+#float
+#height = 25.5
+#print("Your height is " + str(height) + "cm")
+
+#boolean (true or false)
+human = True
+print("Are you a human: " + str(human))

1b13cb1f97948da84b399479e1671ecbd1de9507
Wycliffe Alphus <155804225+WycliffeAlphus@users.noreply.github.com>
2024-01-06 14:23:49 +0300

    Add files via upload

---
+++ Multiple assignment.py
@@ -0,0 +1,19 @@
+#name = "Bro"
+#age = 22
+#Attractive = True
+#name, age, Attractive = "Bro", 22, True
+
+#print(name)
+#print(age)
+#print(Attractive)
+
+# Jane = 30
+# John = 30
+# Peter = 30
+# Ann = 30
+
+Jane = John = Peter = Ann = 30
+print(Jane)
+print(John)
+print(Peter)
+print(Ann)

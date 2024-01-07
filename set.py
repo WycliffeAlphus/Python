2691d29295e1fca5b663ccaff24550a623943367
Wycliffe Alphus <155804225+WycliffeAlphus@users.noreply.github.com>
2024-01-07 12:48:06 +0300

    Add files via upload

---
+++ set.py
@@ -0,0 +1,17 @@
+# set =collection which is unordered, unindexed. No dupicate values
+
+utensils = {"fork", "spoon", "knife"}
+dishes = {"bowl", "plate", "cup", "knife"}
+
+#utensils.add("napkin")
+#utensils.remove("fork")
+#utensils.clear()
+#utensils.update(dishes) # add all elements in dishes to utensils set
+#dinner_table = utensils.union(dishes) # creats a new set and adds the elements in utensils and dishes
+#print(utensils.difference(dishes)) #prints what utensils has that dishes doesn't
+print(utensils.intersection(dishes)) #return the element they have in common
+#for x in utensils:
+ #   print(x)
+
+#for x in dinner_table:
+    #print(x)

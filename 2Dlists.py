804753eb3dd9224e753e56c5ff15ffc70a2d499a
Wycliffe Alphus <155804225+WycliffeAlphus@users.noreply.github.com>
2024-01-07 12:47:25 +0300

    Add files via upload

---
+++ 2Dlists.py
@@ -0,0 +1,10 @@
+#2D lists = a list of separate lists
+
+drinks = ["coffee", "soda", "tea"]
+dinner = ["pizza", "rice", "hotdog"]
+desert = ["cake", "ice cream"]
+
+food = [drinks,dinner,desert] #2D list
+#print(food)
+#print(food[0]) # access the drinks list
+print(food[0][2]) #access the item at index 0 in drinks list

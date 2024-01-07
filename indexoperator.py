18a993883ae11838206100bf8f677a95a2aee539
Wycliffe Alphus <155804225+WycliffeAlphus@users.noreply.github.com>
2024-01-07 12:49:11 +0300

    Add files via upload

---
+++ indexoperator.py
@@ -0,0 +1,16 @@
+#index operator [] = gives access to a sequences element (str,list,tuples)
+
+name = "wycliffe Alphus!"
+#if(name[0].islower()): # checks if the first character is lower case
+#    name = name.capitalize() # then changes the first letter to  capital
+
+first_name = name[0:9].upper()
+last_name = name [9:].lower()
+last_character = name[-1]
+
+print(first_name)   
+print(last_name) 
+print(last_character)
+ 
+ 
+

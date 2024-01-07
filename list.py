44b4ad0a62799d84b7da1287439f1b2a09da84f1
Wycliffe Alphus <155804225+WycliffeAlphus@users.noreply.github.com>
2024-01-07 12:47:02 +0300

    Add files via upload

---
+++ list.py
@@ -0,0 +1,14 @@
+#list = used to store multiple items in a single variable
+
+food =["pizza", "hamburger", "rice", "hotdog"] # this is a list called food, with elements in it
+food[0] = "sushi"
+#print(food[0]) # prints the element at a given index
+#food.append("ice cream") # adds ice cream at the end of the list
+#food.remove("hotdog")
+#food.pop() #removes the last element
+#food.insert(0,"cake") # adds it at a given index, say index 0
+#food.sort() #sorts a list alphabetically
+#food.clear() #clears all elements of a list
+
+for x in food:
+    print(x)

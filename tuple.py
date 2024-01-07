57adffc6e52cb05c4cfe258df44cb88d5ad7d55e
Wycliffe Alphus <155804225+WycliffeAlphus@users.noreply.github.com>
2024-01-07 12:47:40 +0300

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,12 @@
+# tuple = collections which are ordered and unchangeable; used to group together related data
+
+student = ("John", 21, "male")
+
+#print(student.count("John"))
+#print(student.index("male"))
+
+#for x in student:      # print the contents of the tuple
+#    print(x)
+
+if "John" in student:
+    print("John is here")

8c0bd80f8292907ea266da3618e7e66ccacd544d
Wycliffe Alphus <155804225+WycliffeAlphus@users.noreply.github.com>
2024-01-07 12:45:45 +0300

    Add files via upload

---
+++ forloops.py
@@ -0,0 +1,18 @@
+import time
+#for loop = a statement that will execute it's block of code a limited amount of time
+#while loop = unlimited
+#for loop = limited
+
+#for i in range (10):
+ #   print(i+1)
+
+#for i in range (50, 100+1,2):
+ #   print(i)
+
+#for i in "Wycliffe":
+#    print(i)
+
+for  seconds in range(10,0,-1):
+    print(seconds)
+    time.sleep(1)
+print("Happy New Year")

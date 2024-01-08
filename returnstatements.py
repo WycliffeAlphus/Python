f30cc80a976bdd812d2bae905884466a693f3411
Wycliffe Alphus <155804225+WycliffeAlphus@users.noreply.github.com>
2024-01-08 17:17:39 +0300

    Add files via upload

---
+++ returnstatements.py
@@ -0,0 +1,11 @@
+#return statement = functions send Python avlues/objects back to the caller.
+# The values/objects are known as function's return values
+
+def multiply(number1, number2):
+    return number1*number2
+    #result = number1*number2
+    #return result 
+#print(multiply(6,8))
+
+x = multiply(6,8)
+print(x)

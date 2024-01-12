ce72cfca22cbe5db3c2c751e598f25914c42a392
Wycliffe Alphus <155804225+WycliffeAlphus@users.noreply.github.com>
2024-01-12 18:15:43 +0300

    Add files via upload

---
+++ objectorientedprogramming.py
@@ -0,0 +1,13 @@
+#oject is an instance of a class
+from OOP2 import Car
+
+car_1 = Car("Chevy", "Corvette", 2021, "blue")
+car_2 = Car("Ford", "Mustag", 2022, "red")
+
+print(car_2.make)
+print(car_2.model)
+print(car_2.year)
+print(car_2.color)
+
+car_2.drive()
+car_2.stop()

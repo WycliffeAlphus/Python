39d9f58984b30f2481b53f61cd8d2e906539898a
Wycliffe Alphus <155804225+WycliffeAlphus@users.noreply.github.com>
2024-01-12 18:11:07 +0300

    Add files via upload

---
+++ rockpaperscissors.py
@@ -0,0 +1,46 @@
+import random
+
+while True:
+    choices = ["rock", "paper", "scissors"]
+
+    computer = random.choice(choices)
+    player = None
+    while player not in choices:
+        player =input("rock, paper, or scissors?: ").lower() #.lower() takes the user input and makes it all lower case, thus allows inputing any case, as the choices listed are case sensitive
+
+    if player == computer:
+        print("Computer: ", computer)
+        print("Player: ", player)
+        print("Tie!")
+    elif player == "rock":
+        if  computer == "paper":
+            print("Computer: ", computer)
+            print("Player: ", player)
+            print("You lose!")
+        if  computer == "scissors":
+            print("Computer: ", computer)
+            print("Player: ", player)
+            print("You Win!")
+    elif player == "scissors":
+        if  computer == "rock":
+            print("Computer: ", computer)
+            print("Player: ", player)
+            print("You lose!")
+        if  computer == "paper":
+            print("Computer: ", computer)
+            print("Player: ", player)
+            print("You Win!")   
+    elif player == "paper":
+        if  computer == "scissors":
+            print("Computer: ", computer)
+            print("Player: ", player)
+            print("You lose!")
+        if  computer == "rock":
+            print("Computer: ", computer)
+            print("Player: ", player)
+            print("You Win!")
+
+    play_again = input("Play again? (yes/no): ").lower()
+    if play_again != "yes":
+        break
+print("Bye")

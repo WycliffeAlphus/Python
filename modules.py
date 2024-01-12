06264ddf6c4f476c331207f8420a8937d2097e97
Wycliffe Alphus <155804225+WycliffeAlphus@users.noreply.github.com>
2024-01-12 18:09:56 +0300

    Add files via upload

---
+++ modules.py
@@ -0,0 +1,15 @@
+#module = a files containing pythone code. May contain functions, classes, etc
+# used with modular programming, which is to separate a program into parts
+
+#import messages as msg
+
+#msg.hello()
+#msg.bye()
+
+
+#from messages import hello,bye
+
+#hello()
+#bye()
+
+help("modules") #to check all modules in Python
